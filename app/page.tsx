14c271e11525eaffae5b9b1c9ba0802d5b03f476
Dinithi1625403 <dananjanakadd.23@uom.lk>
2026-08-27 23:20:27 +0530

    chore(landing): remove DownloadAppSection from landing page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import TestimonialsSection from "@/components/landing/TestimonialsSection";
 import PricingSection from "@/components/landing/PricingSection";
 import FAQSection from "@/components/landing/FAQSection";
 import CTASection from "@/components/landing/CTASection";
-import DownloadAppSection from "@/components/landing/DownloadAppSection";
 import Footer from "@/components/landing/Footer";
 
 export default function LandingPage() {
@@ -24,7 +23,6 @@ export default function LandingPage() {
       <PricingSection />
       <FAQSection />
       <CTASection />
-      <DownloadAppSection />
       <Footer />
     </div>
   );
